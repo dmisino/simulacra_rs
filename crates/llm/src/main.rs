6b94690503de8714d547c8831925142f9384e9e6
Donnie Misino <donnie.misino@alice-app.com>
2023-04-22 17:01:15 -0700

    Pull api key from environment variable

--- crates/llm/src/main.rs
+++ crates/llm/src/main.rs
@@ -1,9 +1,13 @@
 mod openai;
 use openai::{generate_completion};
+use std::env;
 
 #[tokio::main]
 async fn main() {
-    let api_key = "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";
+    let api_key = match env::var_os("OPENAI_API_KEY") {
+      Some(api_key) => api_key.into_string().unwrap(),
+      None => panic!("OPENAI_API_KEY is not set")
+    };  
     let prompt = "How would you describe quantum mechanics to a computer programmer?";
 
     match generate_completion(prompt, api_key).await {
