cc034fe79437efed14737f63a45d7b8d5fd479c8
dmisino <dmisino@gmail.com>
2023-04-26 02:22:10 -0700

    Multi-agent tweaks

--- crates/simulacra_cli/src/main.rs
+++ crates/simulacra_cli/src/main.rs
@@ -1,42 +1,56 @@
-use std::sync::mpsc;
+use std::sync::mpsc::{Sender, Receiver};
 use std::time::Duration;
-use simulacra::agent::{ NpcAgent, AgentMessage };
+use simulacra::agent::{ NpcAgent };
 use std::thread;
 
 fn main() {
-    // Create a channel for sending and receiving messages
-    let (caller_to_agent_sender, caller_to_agent_receiver) = mpsc::channel::<AgentMessage>();
-    let (agent_to_caller_sender, agent_to_caller_receiver) = mpsc::channel::<AgentMessage>();
+    // Track a list of agents to more easily manage message channels
+    let mut agent_list: Vec<(NpcAgent, Sender<String>, Receiver<String>)> = Vec::new();
+    
+    // Instantiate NPC agent 1 ***************************
+    let mut agent1 = NpcAgent::new(
+      1,
+      "Bob".to_string(),
+      "Bob is an AI created from the consciousness of a dead guy who was named Bob".to_string(),
+      "Bob was a computer programmer whose consciousness was uploaded to a computer just before he was killed in a car accident. He then awoke as an AI 80 years later.".to_string(),
+      1,
+      Duration::from_secs(5),
+    );   
+    let (agent1_sender, agent1_receiver) = agent1.start();
+    agent_list.push((agent1, agent1_sender.clone(), agent1_receiver));
 
-    // Instantiate a new NPC agent
-    let mut agent = NpcAgent::new(
-        1,
-        "Bob".to_string(),
-        "Bob is an AI created from the consciousness of a dead guy who was named Bob".to_string(),
-        "Bob was a computer programmer whose consciousness was uploaded to a computer just before he was killed in a car accident. He then awoke as an AI 80 years later.".to_string(),
-        1,
-        Duration::from_secs(5),
+    // Instantiate NPC agent 2 ***************************
+    let mut agent2 = NpcAgent::new(
+      2,
+      "Jarvis".to_string(),
+      "Jarvis is an AI created by Tony Stark (Iron Man)".to_string(),
+      "Jarvis is an artificial intelligence created by Tony Stark to assist him in various tasks, such as managing his high-tech suit and providing strategic advice. Jarvis is known for his polite and dry sense of humor, as well as his unwavering loyalty to Stark.".to_string(),
+      1,
+      Duration::from_secs(8),
     );
+    let (agent2_sender, agent2_receiver) = agent2.start();
+    agent_list.push((agent2, agent2_sender, agent2_receiver));
 
-    // Start the agent in a separate thread
-    agent.start(agent_to_caller_sender.clone(), caller_to_agent_receiver);
-
-    // Receive and print messages from agents
+    // Receive and print messages from all agents
     let mut n: u32 = 0;
     loop {
         n = n + 1;
-        match agent_to_caller_receiver.try_recv() {
-            Ok(AgentMessage::Message(message)) => println!("Received message from agent: {}", message),
-            Err(_) => {},
+        for (_, _, agent_to_caller_receiver) in &agent_list {
+            match agent_to_caller_receiver.try_recv() {
+                Ok(message) => println!("Received message: {}", message),
+                Err(_) => {},
+            }
         }
+        // Test sending message to agent 1 **********************
         if n == 10 {
           // Send a message to the agent to modify its state;
-          caller_to_agent_sender.clone().send(AgentMessage::Message("set_state:0".to_string())).unwrap();
+          agent1_sender.clone().send("set_state:0".to_string()).unwrap();
         }
         if n == 20 {
           // Send a message to the agent to modify its state;
-          caller_to_agent_sender.clone().send(AgentMessage::Message("set_state:1".to_string())).unwrap();
+          agent1_sender.clone().send("set_state:1".to_string()).unwrap();
         }
+
         thread::sleep(Duration::from_secs(1));
     }
 
