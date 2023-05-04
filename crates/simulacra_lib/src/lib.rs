30b1b45c55c8ba5c199b6ff122d0ef72b455a83e
dmisino <dmisino@gmail.com>
2023-05-04 00:51:53 -0700

    Add workflows, prompt templates, basic processing

---
+++ crates/simulacra_lib/src/lib.rs
@@ -0,0 +1,77 @@
+use std::path::{Path, PathBuf};
+use workflow::*;
+
+// This is the main implementation of the simulacra framework. We
+// will pass a workflow file to the workflow crate and let it run.
+pub fn start(workflow_file: String) {
+    let mut file_path = PathBuf::new();
+    file_path.push(Path::new(env!("CARGO_MANIFEST_DIR")));
+    file_path.push("workflow");
+    file_path.push(workflow_file);
+    workflow::launch_workflow(&file_path.to_str().unwrap());
+    println!("Complete!");
+}
+
+// // Implement a pause duration between calls to the OpenAI API to prevent error 429 - Too many calls
+// pub fn pause() {
+//   std::thread::sleep(std::time::Duration::from_millis(1000));
+// }
+// pub async fn get_new_world_place_npc() -> (String, String, String) {
+//   let prompt = get_prompt("world");
+//   let world: String = if !prompt.is_empty() {
+//       get_chat_completion(prompt).await
+//   } else {
+//       "Error loading prompt from file".to_string()
+//   };
+//   pause();
+//   let prompt = get_prompt("place");
+//   let place: String = if !prompt.is_empty() {
+//       get_chat_completion(prompt.replace("{world}", &world)).await
+//   } else {
+//       "Error loading prompt from file".to_string()
+//   };
+//   pause();
+//   let prompt = get_prompt("npc");
+//   let npc: String = if !prompt.is_empty() {
+//       get_chat_completion(prompt.replace("{world_place}", &format!("{}\\n\\n{}", &world, &place))).await
+//   } else {
+//       "Error loading prompt from file".to_string()
+//   };
+//   pause();
+//   (world, place, npc)
+// }
+
+// pub async fn get_new_world() -> String {
+//   let prompt = get_prompt("world");
+//   let response = if !prompt.is_empty() {
+//       get_chat_completion(prompt).await
+//   } else {
+//       "Error loading prompt from file".to_string()
+//   };
+//   pause();
+//   response
+// }
+
+// pub async fn get_new_place(world: String) -> String {
+//   let prompt = get_prompt("place");
+//   let prompt = prompt.replace("{world}", &world);
+//   let response = if !prompt.is_empty() {
+//       get_chat_completion(prompt).await
+//   } else {
+//       "Error loading prompt from file".to_string()
+//   };
+//   pause();  
+//   response
+// }
+
+// pub async fn get_new_npc(world_place: String) -> String {
+//   let prompt = get_prompt("npc");
+//   let prompt = prompt.replace("{world_place}", &world_place);
+//   let response = if !prompt.is_empty() {
+//       get_chat_completion(prompt).await
+//   } else {
+//       "Error loading prompt from file".to_string()
+//   };
+//   pause();  
+//   response
+// }
